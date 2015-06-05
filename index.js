f13eaff82eec481cf4dc6d845d2ce49dfc531245
coffe67 <coffe67.na@gmail.com>
2015-06-04 23:40:07 -0500

    Adding Handlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 //index.js
 var servidor = require('./server'); //Importamos el módulo servidor creado
 var router = require('./router'); //Importamos el módulo router
-servidor.init(); //Llama al método inicializar del objeto servidor
+var handlers = require('./handler'); //Importamos el módulo manejador
+var handler = {}
+handler["/"] = handlers.funcion_1;
+handler["/funcion_1"] = handlers.funcion_1;
+handler["/funcion_2"] = handlers.funcion_2;
+
+servidor.init(router.route, handler); //Llama al método inicializar del objeto servidor
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //Servidor.js
-function init () {
+function init (route, handler) {
 	var server = require('http').createServer();
-	function control(petic, resp) {
-		var pathName = url.parse(petic.url).pathname;
-		route(pathName);
+	function control(request, response) {
+		var pathName = url.parse(request.url).pathname;
+		route(handler,pathName,response);
 		console.log('Petición recibida'); //Texto que saldrá por consola
-		resp.writeHead(200, {'content-type': 'text/plain'});
-		resp.write('Peticion recibida en el servidor.');
-		resp.end();
+		response.writeHead(200, {'content-type': 'text/plain'});
+		respose.write('Peticion recibida en el servidor.');
+		response.end();
 	}
 	server.on('request', control).listen(8080);
 	console.log('Servidor inicializado');
