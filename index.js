f138ce101d46be27e57099c21bb18c922ca6cf9d
coffe67 <coffe67.na@gmail.com>
2015-06-05 00:08:19 -0500

    Fix Handler Error

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var servidor = require('./server'); //Importamos el módulo servidor creado
 var router = require('./router'); //Importamos el módulo router
 var handlers = require('./handler'); //Importamos el módulo manejador
-var handler = {}
+var handler = {} // Add URLS  URL(PATH, VIEW,)
 handler["/"] = handlers.funcion_1;
-handler["/funcion_1"] = handlers.funcion_1;
+handler["/:var"] = handlers.funcion_1;
 handler["/funcion_2"] = handlers.funcion_2;
 
 servidor.init(router.route, handler); //Llama al método inicializar del objeto servidor
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //Servidor.js
 function init (route, handler) {
 	var server = require('http').createServer();
+	var url = require('url');
 	function control(request, response) {
 		var pathName = url.parse(request.url).pathname;
-		route(handler,pathName,response);
 		console.log('Petición recibida'); //Texto que saldrá por consola
+		route(handler,pathName,response);
 		response.writeHead(200, {'content-type': 'text/plain'});
-		respose.write('Peticion recibida en el servidor.');
-		response.end();
+		//response.write('Peticion recibida en el servidor.');
+		//response.end();
 	}
 	server.on('request', control).listen(8080);
 	console.log('Servidor inicializado');
